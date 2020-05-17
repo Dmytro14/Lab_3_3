480e8084709783ce89dd43261ba5cf9344c6a7c5
Dmytro14 <60977979+Dmytro14@users.noreply.github.com>
2020-05-17 20:00:15 +0300

    first test

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fromFile = new StreamReader("D:/C#_2/input.txt");
+
+            string str = fromFile.ReadToEnd();
+
+            string[] words = str.Split(new char[] { ' ', ':', ';', '.', ',', '?', '!'}, StringSplitOptions.RemoveEmptyEntries);
+
+            StreamWriter toFile = File.CreateText("D:/C#_2/output.txt");
+
+            toFile.WriteLine(words.Length);
+
+            fromFile.Close();
+
+            toFile.Close();
+        }
+    }
+}
